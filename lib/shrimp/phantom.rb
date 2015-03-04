3bc424bc81e95636f882d763ca59d1655d24908f
Marwan Rabbâa <marwan.rabbaa@tech4team.fr>
2015-03-04 12:53:11 +0100

    do not escape shell args

--- lib/shrimp/phantom.rb
+++ lib/shrimp/phantom.rb
@@ -59,8 +59,8 @@ module Shrimp
       rendering_time, timeout           = options[:rendering_time], options[:rendering_timeout]
       viewport_width, viewport_height   = options[:viewport_width], options[:viewport_height]
       max_redirect_count                = options[:max_redirect_count]
-      header		                = Regexp.escape(options[:header])
-      footer		                = Regexp.escape(options[:footer])
+      header		                = "'#{options[:header]}'"
+      footer		                = "'#{(options[:footer])}'"
       @outfile                          ||= "#{options[:tmpdir]}/#{Digest::MD5.hexdigest((Time.now.to_i + rand(9001)).to_s)}.pdf"
       command_config_file               = "--config=#{options[:command_config_file]}"
       [
